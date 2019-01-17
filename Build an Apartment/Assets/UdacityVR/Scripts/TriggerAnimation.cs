72ef4a0fb2c1a91c8ad9f9a8e209807c13877d23
Kristin Dragos <KristinDragos@gmail.com>
2019-01-17 16:15:22 -0700

    chore: Update TriggerAnimation script
    Update TriggerAnimation.cs to pull animator from the components on the
    object that the script is attach to.
    Update TriggerAnimation.cs with warnings about variables that aren't
    set properly.

--- Build an Apartment/Assets/UdacityVR/Scripts/TriggerAnimation.cs
+++ Build an Apartment/Assets/UdacityVR/Scripts/TriggerAnimation.cs
@@ -15,7 +15,23 @@ public class TriggerAnimation : MonoBehaviour
 	{
 		// If the player pressed the cardboard button (or touched the screen), set the trigger parameter to active (until it has been used in a transition)
 		if (Input.GetMouseButtonDown (0)) {
-			animator.SetTrigger (triggerName);
+            if (animator == null) {
+                Debug.LogWarning("Please add an animator to your " + gameObject.name);
+                return;
+            }
+            if (triggerName == null) {
+                Debug.LogWarning("What's the trigger parameter name you want to call? Please add it to the TriggerAnimation script on your " + gameObject.name);
+                return;
+            }
+            animator.SetTrigger (triggerName);
 		}
 	}
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        if(animator == null) {
+            Debug.LogWarning("Please add an animator to your " + gameObject.name);
+        }
+    }
 }
